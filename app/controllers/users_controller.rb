9488bf4fdfd3747194d7047e5d517e2983352e83
Ryan Keiper <ryankeiper@gmail.com>
2015-05-11 14:31:41 -0400

    added hand rolled authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+	before_action :require_signin, except: [:new]
+
 	def index
 		@users = User.all
 	end
@@ -14,6 +16,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			sign_in @user
 			redirect_to @user
 		else
 			render :new
